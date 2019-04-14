e0c2ad38dcc66f23ef0e07233b319ae5cbcf5675
edwardsun007 <edwardsun007@gmail.com>
2019-04-13 20:48:14 -0700

    social inputs component created

--- client/src/components/create-profile/createProfile.js
+++ client/src/components/create-profile/createProfile.js
@@ -60,6 +60,42 @@ class createProfile extends Component {
             onChange={this.onChange}
             error={errors.twitter}
           />
+
+          <InputGroup
+            placeholder="Facebook Page URL"
+            name="facebook"
+            icon="fab fa-facebook"
+            value={this.state.facebook}
+            onChange={this.onChange}
+            error={errors.facebook}
+          />
+
+          <InputGroup
+            placeholder="LinkedIn Profile URL"
+            name="linkedin"
+            icon="fab fa-linkedin"
+            value={this.state.linkedin}
+            onChange={this.onChange}
+            error={errors.linkedin}
+          />
+
+          <InputGroup
+            placeholder="Youtube Channel URL"
+            name="youtube"
+            icon="fab fa-youtube"
+            value={this.state.youtube}
+            onChange={this.onChange}
+            error={errors.youtube}
+          />
+
+          <InputGroup
+            placeholder="Instagram URL"
+            name="instagram"
+            icon="fab fa-instagram"
+            value={this.state.instagram}
+            onChange={this.onChange}
+            error={errors.instagram}
+          />
         </div>
       );
     }
